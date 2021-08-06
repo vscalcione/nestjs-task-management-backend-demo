bf7cfbf20a2a60ea90f3d556817a715e0eb10fb6
vscalcione <vincenzo99.scalcione@gmail.com>
2021-08-06 11:46:05 +0200

    Refactoring code

---
+++ src/controllers/tasks.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { TasksService } from '../services/tasks.service';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private service: TasksService) {}
+
+  @Get()
+  getAllTasks = () => {
+    return this.service.getAllTasks();
+  };
+}
